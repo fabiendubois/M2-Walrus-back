badee39363df870d69b8c71b77b2e2814dbe8a63
Fabien <fabien@mbp-de-fabien.lan>
2019-01-15 21:53:34 +0100

    FDU : Init Commit

---
+++ src/main/java/com/example/walrus/repository/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.example.walrus.repository;
+
+import com.example.walrus.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/example/walrus/repository/QuizRepository.java
@@ -0,0 +1,7 @@
+package com.example.walrus.repository;
+
+import com.example.walrus.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+}
